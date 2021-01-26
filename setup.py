5f89b60d8519e1221a4cbbb25a07db81b0cd9f38
Susrutha Gongalla <susruthagongalla@Susruthas-MacBook-Pro.local>
2021-01-25 22:55:08 -0800

    adding template code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a model to predict the presence of heart disease',
+    author='Susrutha Gongalla',
+    license='MIT',
+)
